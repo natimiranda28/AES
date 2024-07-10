ab30b712ac9fbe4644b14a70d6cbaf7e26d4eb13
Natalia Miranda <natumiranda.28@gmail.com>
2024-07-10 19:51:34 -0300

    Initial commit

---
+++ Aes/src/main/java/com/aes/Aes/AesApplication.java
@@ -0,0 +1,13 @@
+package com.aes.Aes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AesApplication.class, args);
+	}
+
+}
